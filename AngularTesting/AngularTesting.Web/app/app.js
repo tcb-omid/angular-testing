4d093b258e5098a4572fc341af6517a925aa7afa
Omid Sharafzadeh <omid.sharafzadeh@topcashback.co.uk>
2016-11-15 17:09:12 +0000

    Initial commit.

---
+++ AngularTesting/AngularTesting.Web/app/app.js
@@ -0,0 +1,23 @@
+﻿(function () {
+
+    var app = angular.module('app', []);
+    
+    app.service('calculator', function () {
+
+        return {
+            add: add,
+            subtract: subtract
+        };
+
+        function add(x, y) {
+            return x + y;
+        }
+
+        function subtract(x, y) {
+            return x - y;
+        }
+
+    });
+
+    }
+)();
---
+++ AngularTesting/AngularTesting.Web/app/tests/calculator.spec.js
@@ -0,0 +1,26 @@
+﻿(function() {
+    describe('calculator', function () {
+        var calculator, httpBackend;
+        //2.
+        beforeEach(function () {
+            //3. load the module.
+            module('app');
+
+            inject(function ($httpBackend, _calculator_) {
+                calculator = _calculator_;
+                httpBackend = $httpBackend;
+            });
+        });
+
+        afterEach(function () {
+        });
+
+        //6.
+        it('ServiceTestSpec', function () {
+
+            var returnedPromise = calculator.add(2, 1);
+            expect(returnedPromise).toEqual(3);
+
+        });
+    });
+})();
