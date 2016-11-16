182f9c6a6878faa38596076b1e9d24816c0d2b27
Omid Sharafzadeh <omid.sharafzadeh@topcashback.co.uk>
2016-11-16 10:24:04 +0000

    jhujh

--- AngularTesting/AngularTesting.Web/karma.conf.js
+++ AngularTesting/AngularTesting.Web/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: '.',
 
 
     // frameworks to use
@@ -15,10 +15,10 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        "./bower_components/angular/angular.js",
-        "./bower_components/angular-mocks/angular-mocks.js",
-        "./app/tests/calculator.spec.js",
-        "./app/app.js"
+        "bower_components/angular/angular.js",
+        "bower_components/angular-mocks/angular-mocks.js",
+        "app/tests/calculator.spec.js",
+        "app/app.js"
     ],
 
 
