9625e91aa99bf79d09cff3341fee2bcff1624ac2
Omid Sharafzadeh <omid.sharafzadeh@topcashback.co.uk>
2016-11-16 10:19:16 +0000

    uej

--- AngularTesting/AngularTesting.Web/karma.conf.js
+++ AngularTesting/AngularTesting.Web/karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['teamcity'],
 
 
     // web server port
@@ -58,12 +58,12 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
